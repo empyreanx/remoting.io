86b876de3dae23ed001dda6ef3348c8c90eb0f62
James McLean <james@mcleanlabs.ca>
2015-02-17 12:40:25 -0500

    Added instantiation request handling.

--- test/server.js
+++ test/server.js
@@ -2,6 +2,7 @@
 
 var events = require('events');
 var heir = require('heir');
+var expect = require('expect.js');
 
 var Server = require('../lib/server');
 
@@ -15,21 +16,54 @@ function MockSocket () {
 
 heir.mixin(MockSocket, events.EventEmitter.prototype);
 
-//function MockService(session) {
-//	this.session = session;
-//}
+function TestService1() {
+	this.session['greeting'] = 'Hello world!';
+}
+
+TestService1.exports = [ 'test1', 'test2' ];
+
+function TestService2() {	
+}
+
+TestService2.exports = [ 'test1', 'test2' ];
 
 describe('server', function () {
-	var socketServer;
-	var remotingServer;
+	var socketServer, socket, remoting;
 	
 	beforeEach(function (done) {
 		socketServer = new MockSocketServer();
-		remotingServer = new Server(socketServer);
+		socket = new MockSocket();
+		
+		remoting = new Server(socketServer);
+		remoting.remote('TestService1', TestService1);
+		remoting.remote('TestService2', TestService2);
+		
+		socketServer.emit('connection', socket);
+		
 		done();
 	});
 	
-	it('should be ok', function () {
+	it('should send service list', function (done) {
+		var request = { id: 0, type: 'list' };
+		var response = { id: 0, type: 'list', list: ['TestService1', 'TestService2']}; 
 		
+		socket.send = function (message) {
+			expect(message).to.be(JSON.stringify(response));
+			done();
+		};
+		
+		socket.emit('message', JSON.stringify(request));
+	});
+	
+	it('should instantiate service', function (done) {
+		var request = { id: 0, type: 'new', service: 'TestService1' };
+		var response = { id: 0, type: 'new', instance: 0, exports: ['test1', 'test2'] };
+		
+		socket.send = function (message) {
+			expect(message).to.be(JSON.stringify(response));
+			done();
+		};
+		
+		socket.emit('message', JSON.stringify(request));
 	});
 });
